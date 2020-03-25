71183186f4e5d14da59e412916afafdb475cf3d0
LampV <1071952403@qq.com>
2020-03-25 17:05:15 +0800

    注释更新

--- ns3-addon/ns3-src/action-executor/model/ipv4-rl-routing.cc
+++ ns3-addon/ns3-src/action-executor/model/ipv4-rl-routing.cc
@@ -1,20 +1,10 @@
-// -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*-
-//
-// Copyright (c) 2008 University of Washington
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License version 2 as
-// published by the Free Software Foundation;
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
+/*
+ * @author: Jiawei Wu
+ * @create time: 2020-03-17 20:52
+ * @edit time: 2020-03-25 17:02
+ * @file: /ns3-addon/ns3-src/action-executor/model/ipv4-rl-routing.cc
+ */
+
 
 #include <vector>
 #include <iomanip>
