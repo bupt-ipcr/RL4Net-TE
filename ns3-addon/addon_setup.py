e79f1a076cdb55a0fbd0c3c505b4981d6ea5809c
LampV <1071952403@qq.com>
2020-03-29 16:18:29 +0800

    remove useless code, re-format code

--- ns3-addon/addon_setup.py
+++ ns3-addon/addon_setup.py
@@ -3,56 +3,45 @@
 """
 @author: Jiawei Wu
 @create time: 2020-02-18 19:56
-@edit time: 2020-02-28 23:02
-@desc: 将精简包安装到原ns3的代码中
+@edit time: 2020-03-29 16:17
+@desc: 将addon的源码安装到原ns3的代码中
 """
+
 import argparse
 import json
 import os
 from pathlib import Path
 
-
+# 配置执行参数
 parser = argparse.ArgumentParser(description='安装参数')
 parser.add_argument('--wafdir', default='/ns-3-dev', type=str, help='安装目录')
-parser.add_argument('--usepip3', '-pip3', default=False, action='store_true', help='是否使用pip3安装ns3gym')
 
 # build及参数
-parser.add_argument('--build', default=False, action='store_true', help='是否执行build代码')
 parser.add_argument('--noconfirm', default=False, action='store_true', help='是否跳过环境检测')
 parser.add_argument('--nocopy', default=False, action='store_true', help='是否跳过文件复制')
 parser.add_argument('--nowscript', default=False, action='store_true', help='是否跳过wscript覆盖')
 parser.add_argument('--norebuild', default=False, action='store_true', help='是否跳过waf rebuild')
 parser.add_argument('--noreconf', default=False, action='store_true', help='是否跳过waf reconf')
 
-# update及参数
-parser.add_argument('--update', default=False, action='store_true', help='是否执行update代码')
-
-# clean及参数
-parser.add_argument('--clean', default=False, action='store_true', help='是否执行clean代码')
-
 args = parser.parse_args()
 
 cur_path = Path().resolve()
 ns3_path = (cur_path / args.wafdir).resolve()
 dir_dict = {
-            'action-executor': 'internet',
-            'metric-extractor': 'flow-monitor'
-        }
+    'action-executor': 'internet',
+    'metric-extractor': 'flow-monitor'
+}
 
 
 def env_confirm():
-    """
-    检查安装环境
-    """
+    """检查安装环境"""
     # 确保安装目录存在
     if not ns3_path.exists():
         raise TypeError("安装目录不存在")
 
 
 def file_copy():
-    """
-    进行文件复制操作
-    """
+    """进行文件复制操作"""
     # 将rapidjson复制到ns3文件夹下
     os.system(f"cp -r rapidjson/ {ns3_path.resolve()}")
 
@@ -79,10 +68,10 @@ def file_copy():
         os.system(f'cp -r {program_path.resolve()} {scratch_path.resolve()}')
 
 
-def wscript_append(waf_script, module_path):
-    """
-    向wscript文件中添加相应文件的编译需求
-    @param waf_script: str类型的wscript
+def wscript_append(waf_script: str, module_path: str):
+    """向wscript文件中添加相应文件的编译需求
+    @param waf_script: 读取的wafscript文件内容
+    @param module_path: 与要修改的wscript对应的module的路径（在addon文件夹下）
     """
     replace_str = ''
     ws_lines = waf_script.split('\n')
@@ -116,33 +105,28 @@ def wscript_append(waf_script, module_path):
 
 
 def wscript_rewrite():
+    """修改wscript文件
+    将新增的文件写入wscript的编译列表，保证新增文件被编译
     """
-    进行文件复制操作
-    """
-    # 将rapidjson复制到ns3文件夹下
-    os.system(f"cp -r rapidjson/ {ns3_path.resolve()}")
 
-    # 修改对应的wafscript
+    # 遍历ns3-src下的module，修改对应的wafscript
     src_path = ns3_path / 'src'
     ns3src_path = cur_path / 'ns3-src'
 
-    for module_path in ns3src_path.iterdir():   # 遍历ns3src下的目录
+    for addon_module_path in ns3src_path.iterdir():   # 遍历ns3src下的目录
+        module_name = dir_dict[addon_module_path.parts[-1]]         # 获取module的相对路径，即module名称
+        wscript_path = src_path / module_name / 'wscript'     # 获取wscript在ns3_path下的对应路径
 
-        # 对于不是opengym的文件夹而言，还需要修改对应wafscript文件
-        if module_path != ns3src_path / 'opengym':
-
-            relative_path = dir_dict[module_path.parts[-1]]         # 获取module的相对路径
-            wscript_path = src_path / relative_path / 'wscript'     # 获取wscript在ns3_path下的对应路径
-
-            # 先读取这个文件的内容
-            waf_script = (wscript_path.read_text())
-            # 添加编译信息，获取新的scrpte字符串
-            new_wscript = wscript_append(waf_script, module_path)
-            # 新字符串写入文件
-            wscript_path.write_text(new_wscript)
+        # 先读取这个文件的内容
+        waf_script = (wscript_path.read_text())
+        # 添加编译信息，获取新的scrpte字符串
+        new_wscript = wscript_append(waf_script, addon_module_path)
+        # 新字符串写入文件
+        wscript_path.write_text(new_wscript)
 
 
 def waf_reconf():
+    """执行waf configure指令"""
     # 定位waf
     waf_path = ns3_path / 'waf'
     os.chdir(ns3_path.resolve())
@@ -152,6 +136,7 @@ def waf_reconf():
 
 
 def waf_rebuild():
+    """执行waf build指令"""
     # 定位waf
     waf_path = ns3_path / 'waf'
     os.chdir(ns3_path.resolve())
@@ -160,10 +145,9 @@ def waf_rebuild():
     os.system(f'{waf_path} build')
 
 
-def build():
+def install():
     """
-    build ns3-gym 环境，包括：
-    - 产生ns3gym的wafPath配置文件
+    安装 ns3-addon，包括：
     - 确认目标文件夹存在
     - ns3源文件拷贝
     - 更新相关module的wscript文件
@@ -183,33 +167,5 @@ def build():
     print('安装完成')
 
 
-def update():
-    """更新sim文件"""
-    # 将scratch文件夹下cpp部分复制到对应目录下
-    simulator_path = cur_path / 'simulator'
-    scratch_path = ns3_path / 'scratch'
-
-    for program_path in simulator_path.iterdir():     # 遍历所有要被复制的模拟器
-        # 每个program都要被复制到 ns3_path/scratch 下
-        os.system(f'cp -r {program_path.resolve()} {scratch_path.resolve()}')
-
-    print('更新完成')
-
-
-def clean():
-    """清除无效文件"""
-    # 删除ns3src文件夹
-    os.system(f'rm -rf {cur_path.resolve()}/ns3src')
-    # 删除rapidjson文件夹
-    os.system(f'rm -rf {cur_path.resolve()}/rapidjson')
-
-    print('清理完成')
-
-
 if __name__ == '__main__':
-    if args.build:
-        build()
-    if args.update:
-        update()
-    if args.clean:
-        clean()
+    install()
